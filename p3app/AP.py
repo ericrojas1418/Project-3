a6b30f307c2aa323c69215c4600ca08e99d3d8ca
Eric Rojas <rojase10@uci.edu>
2024-09-06 06:24:35 -0700

    Creating the logging capability for our connect method. Included the feature/ability for our class object to deny a connection if our device limit was met

--- p3app/AP.py
+++ p3app/AP.py
@@ -26,24 +26,34 @@ class AP:
         self.network_knowledge = {
 
         }
+        self.current_device_count = 0
         self.device_rssi = {}
         #method to call method to update dictionary of device_rssi
 
     #this will come in helpful after movements
     def add_device_to_network(self,new_device): #To calculate signal strength, we inbed the two necessary functions, bc otherwise would be redundant
+
+        self.currentStep += 1 # step counter is incremeneted
         def calc_rssi(device):
 
-            def calculate_device_distance_from_ap(device):
-                distance = math.sqrt(((int(device.get_x()) - int(self.x_coord)) ** 2) + ((int(device.get_y()) - int(self.y_coord)) ** 2))
+            def calculate_device_distance(device):
+                distance = math.sqrt(((int(device.get_x()) - int(self.x_coord)) ** 2) +
+                                     ((int(device.get_y()) - int(self.y_coord)) ** 2))
                 return distance
 
-            device_rssi = int(self.power_level) - ((20 * math.log10(calculate_device_distance_from_ap(device))) -
+            device_rssi = int(self.power_level) - ((20 * math.log10(calculate_device_distance(device))) -
                                                    (20 * math.log10(eval(self.frequency))) - 32.44)
             return device_rssi
+
+        if self.current_device_count == self.device_limit: #if we are out of potential connections, we will deny the connection
+            self.AP_log.append(f'Step {self.currentStep}: {new_device.get_name()} TRIED {self.AP_name} BUT WAS DENIED')
+            return
 
         if isinstance(new_device, Device): #If we try to add a device, we make a dictionary of the device's information, and update our existing dictionary to include it
             signal_strength = calc_rssi(new_device)
             self.network_devices.update({new_device.get_name() : [new_device.get_x(),new_device.get_y(),new_device.get_standard(), new_device.get_standard(), new_device.get_speed(), new_device.get_supports_11k(), new_device.get_supports_11v(), new_device.get_supports_11r(), new_device.get_minimal_rssi(), signal_strength]}) #Information will be stored in this order. x_coord, y_coord, standard,speed, supports_11k, supports_11v, supports_11r, minimal_rssi required, SIGNAL STRENGTH
+            self.AP_log.append(f'Step {self.currentStep}: {new_device.get_name()} CONNECT LOCATION {new_device.get_x()} {new_device.get_y()} {new_device.get_standard()} {new_device.get_speed()} {new_device.get_supports_11k()} {new_device.get_supports_11v()} {new_device.get_supports_11r()}')
+            self.current_device_count += 1
         else:
             raise ValueError("Can only Device type objects to our Device Network")
 
--- tests/p3app/AP_test.py
+++ tests/p3app/AP_test.py
@@ -3,14 +3,38 @@ from p3app.AP import *
 from p3app.Device import *
 
 class MyTestCase(unittest.TestCase):
+    test_counter = 0
     def setUp(self):
         self.test_AP = AP("AP1", "0", "0", "6","20", "2.4/5", "WiFi6", "true", "true", "true", "50", "10", "75")
         self.test_device1= Device("Client1", "10", "10", "WiFi6", "2.4/5", "true", "true", "true", "73")
-
+        self.test_device2 = Device("Client2", "9", "8", "WiFi7", "2.4/4", "true", "true", "true", "73")
+        MyTestCase.test_counter += 1
+        print(f'beginning test number {MyTestCase.test_counter}')
     def test_device_adding_works(self):
         self.test_AP.add_device_to_network(self.test_device1)
         print(self.test_AP.network_devices)
         assert len(self.test_AP.network_devices) == 1
 
+    def test_device_connect_log_works(self):
+        self.test_AP.add_device_to_network(self.test_device1)
+        print(self.test_AP.AP_log)
+        assert len(self.test_AP.AP_log) == 1
+
+    def test_device_connect_log_works2(self):
+        self.test_AP.add_device_to_network(self.test_device1)
+        self.test_AP.add_device_to_network(self.test_device2)
+        print(self.test_AP.AP_log)
+        assert len(self.test_AP.AP_log) == 2
+
+    def test_device_denial_works(self):
+        self.test_AP.device_limit = 1
+        self.test_AP.add_device_to_network(self.test_device1)
+        self.test_AP.add_device_to_network(self.test_device2)
+        print(self.test_AP.AP_log)
+        assert len(self.test_AP.AP_log) == 2
+
+    def tearDown(self):
+        print(f'Exiting test number {MyTestCase.test_counter}')
+        print()
 if __name__ == '__main__':
     unittest.main()
